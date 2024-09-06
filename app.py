92219b00c2278f505745a62e1e6ed92c2d7190e6
Pilla Shanmukha Dhanush <120253955+shan4smiles@users.noreply.github.com>
2024-09-07 01:08:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 import os
-os.environ["OPENAI_API_KEY"] = "#your_key"
+os.environ["OPENAI_API_KEY"] = "sk...Qkm"
 from gtts import gTTS  # new import
 from io import BytesIO  # new import
 
